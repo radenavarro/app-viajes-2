424d608e64a3c1c314eab3452f8f3293476073d6
Jessica Melero <jessicameleromartinez@MacBook-Pro-de-Jessica-4.local>
2018-11-28 16:18:10 +0100

    Registro completado, añadidas las opciones en la barra, falta que se redirijan los enlaces

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/inicio', function(req, res, next) {
-//   res.render('plantilla.hbs', { titulo: 'Express' });
-// });
+router.get('/', function(req, res, next) {
+  res.render('plantilla.hbs', { titulo: 'Express' });
+});
 // Ruta para acceder al login
 router.get('/login', function(req, res, next) {
   res.render('login.hbs', {titulo: 'LOGIN'});
+});
+// Ruta para acceder al registro
+router.get('/registro', function(req, res, next) {
+  res.render('registro.hbs', {titulo: 'REGISTRO'});
 });
 
 module.exports = router;
