cdff49040b71e0516913118b453715f3cb942f11
Raul De la Paz <rauldelapaznavarro@gmail.com>
2019-01-09 14:50:50 +0100

    Corrección rutas, mysql

--- helpers/mysqlConnection.js
+++ helpers/mysqlConnection.js
@@ -1,15 +1,24 @@
-const Mysql = require('mysql');
-const bdconfig = require('../config');
+const Sequelize = require('sequelize');
+// const Mysql = require('mysql');
+const ConnConfig = require('../config');
 
-class mysqlConnection {
-    static getConnection(){
-        return Mysql.createConnection({
-            host: bdconfig.mysql.host,
-            user: bdconfig.mysql.user,
-            password: bdconfig.mysql.password,
-            database: bdconfig.mysql.database
-        })
-    }
-}
+const SequelizeConn = new Sequelize(ConnConfig.configBD.database, ConnConfig.configBD.user, ConnConfig.configBD.password, {
+    host: ConnConfig.configBD.host,
+    dialect: ConnConfig.configBD.dialect,
+    port: ConnConfig.configBD.port
+});
 
-module.exports = mysqlConnection;
+module.exports = SequelizeConn;
+
+// class mysqlConnection {
+//     static getConnection(){
+//         return Mysql.createConnection({
+//             host: bdconfig.mysql.host,
+//             user: bdconfig.mysql.user,
+//             password: bdconfig.mysql.password,
+//             database: bdconfig.mysql.database
+//         })
+//     }
+// }
+//
+// module.exports = mysqlConnection;
